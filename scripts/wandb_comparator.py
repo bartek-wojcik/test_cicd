c842eb2522a3a3d12f51cf9838814f3da681f26f
Bartek Wójcik <bartekwwb@gmail.com>
2024-10-30 20:43:17 +0100

    Update wandb_comparator.py

--- scripts/wandb_comparator.py
+++ scripts/wandb_comparator.py
@@ -4,7 +4,7 @@ import os
 import argparse
 import wandb
 from ghapi.all import GhApi
-import wandb_workspaces.reports as wr
+import wandb_workspaces.reports.v2 as wr
 
 def main():
     # Parse arguments from the command line
